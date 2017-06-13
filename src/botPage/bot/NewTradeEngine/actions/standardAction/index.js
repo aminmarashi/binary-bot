a3a6949cbd8c7ca3ecfbc59418a148c8d0d92fc2
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-13 16:59:27 +0800

    Extend standard action functionality to support error

--- src/botPage/bot/NewTradeEngine/actions/standardAction/index.js
+++ src/botPage/bot/NewTradeEngine/actions/standardAction/index.js
@@ -1,6 +1,15 @@
 export default function standardAction(type) {
-    return payload => ({
-        type,
-        payload,
-    });
+    return (payload, error = false) => {
+        const baseAction = {
+            type,
+            payload,
+        };
+        if (error) {
+            return {
+                ...baseAction,
+                error: true,
+            };
+        }
+        return baseAction;
+    };
 }
--- src/botPage/bot/NewTradeEngine/actions/standardAction/index.spec.js
+++ src/botPage/bot/NewTradeEngine/actions/standardAction/index.spec.js
@@ -3,12 +3,21 @@ import standardAction from './';
 const type = 'some type';
 const actionCreator = standardAction(type);
 const payload = {};
+const errorPayload = Error('some msg');
+const error = true;
 
 describe('standardAction action creator', () => {
     it('should return an action creator with just payload (no error handling)', () => {
         expect(actionCreator(payload)).toEqual({
             type,
             payload,
+        });
+    });
+    it('should return an action creator with payload and error', () => {
+        expect(actionCreator(errorPayload, error)).toEqual({
+            type,
+            payload: errorPayload,
+            error,
         });
     });
 });
