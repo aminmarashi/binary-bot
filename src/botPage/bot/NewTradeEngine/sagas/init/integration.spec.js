881dbe695997f754cd7f1ab42bff54dc3f67e5e1
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-13 16:27:21 +0800

    Fix init integration test

--- src/botPage/bot/NewTradeEngine/sagas/init/integration.spec.js
+++ src/botPage/bot/NewTradeEngine/sagas/init/integration.spec.js
@@ -1,17 +1,13 @@
 import * as matchers from 'redux-saga-test-plan/matchers';
 import { expectSaga } from 'redux-saga-test-plan';
 import { eventChannel } from 'redux-saga';
+import requestBalance from '../balance/requestBalance';
+import createScope from '../createScope';
+import { initialize } from '../../actions/standard';
 import * as actions from '../../constants/actions';
 import * as properties from '../../constants/properties';
 import { updatePropertyAction } from '../../tools';
-import requestBalance from '../balance/requestBalance';
-import createScope from '../createScope';
-import balance from '../balance';
-import { initialize } from '../../actions/standard';
-
 import init from './';
-import tick from '../tick';
-// import balance from '../balance';
 import dataStream from '../dataStream';
 
 const symbol = 'R_100';
@@ -25,21 +21,38 @@ const fakeBalanceChannel = eventChannel(() => () => {});
 const epoch = 123456;
 const tickPayload = { epoch };
 const balancePayload = { balance: '12.00', currency: 'USD' };
+let firstTick = true;
+let firstBalance = true;
 
 describe('init integration', () => {
     it('should put INITIALIZE', () =>
         expectSaga(init, arg)
             .provide([
-                [matchers.spawn.fn(balance), {}],
-
-                [matchers.take(updatePropertyAction(properties.RECEIVED_BALANCE)), {}],
-                [matchers.take(fakeTickChannel), tickPayload],
-                // [matchers.call.fn(requestBalance), {}],
-                // [matchers.call(dataStream, { $scope, type: 'balance' }), fakeBalanceChannel],
+                [matchers.call.fn(requestBalance), {}],
+                [matchers.call(dataStream, { $scope, type: 'balance' }), fakeBalanceChannel],
                 [matchers.call(dataStream, { $scope, type: 'tick', symbol }), fakeTickChannel],
+                {
+                    take({ channel }, next) {
+                        if (channel === fakeTickChannel) {
+                            if (firstTick) {
+                                firstTick = false;
+                                return tickPayload;
+                            }
+                            return undefined;
+                        }
+                        if (channel === fakeBalanceChannel) {
+                            if (firstBalance) {
+                                firstBalance = false;
+                                return balancePayload;
+                            }
+                            return undefined;
+                        }
+                        return next();
+                    },
+                },
             ])
             .take(actions.NEW_TICK)
-            // .take(updatePropertyAction(properties.RECEIVED_BALANCE))
+            .take(updatePropertyAction(properties.RECEIVED_BALANCE))
             .put(initialize(initOption))
             .run());
 });
--- src/botPage/bot/NewTradeEngine/sagas/tick/tickLoop/index.js
+++ src/botPage/bot/NewTradeEngine/sagas/tick/tickLoop/index.js
@@ -2,10 +2,7 @@ import { take, put } from 'redux-saga/effects';
 import { newTick } from '../../../actions/standard';
 
 export default function* tickLoop(channel) {
-    console.log(channel);
-
     let payload = yield take(channel);
-    console.log(payload);
     while (payload) {
         const { epoch } = payload;
         yield put(newTick(epoch));
